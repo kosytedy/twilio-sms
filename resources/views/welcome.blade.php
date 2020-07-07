b154b88af496fa9641c6e032d623e97c29918fe0
kosytedy <kosytedy@yahoo.com>
2020-07-07 22:57:17 +0100

    Added SMS form

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,96 +4,63 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
-
+        <title>Laravel twilio bulk SMS</title>
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
+        <div class="container" style="margin-top:50px;">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card" style="color:#fff;background-color:green;">
+                        <div class="card-header" style="border-bottom:2px solid #ffc107">{{ __('Send Bulk SMS') }}</div>
+        
+                        <div class="card-body">
+                            <form method="POST" action="{{ route('send') }}">
+                                @csrf
+        
+                                <div class="form-group row">
+                                    <label for="numbers" class="col-md-4 col-form-label text-md-right">{{ __('Enter numbers') }}</label>
+        
+                                    <div class="col-md-8">
+                                        <textarea id="numbers" cols="50" rows="7" placeholder="Enter numbers in international format and in comma seperated values. eg. +1234567890,+2323232320" class="form-control{{ $errors->has('numbers') ? ' is-invalid' : '' }}" name="numbers" value="{{ old('numbers') }}" required autofocus></textarea>
+        
+                                        @if ($errors->has('numbers'))
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $errors->first('numbers') }}</strong>
+                                            </span>
+                                        @endif
+                                    </div>
+                                </div>
+        
+                                <div class="form-group row">
+                                    <label for="message" class="col-md-4 col-form-label text-md-right">{{ __('Message') }}</label>
+        
+                                    <div class="col-md-8">
+                                        <textarea id="message" placeholder="Enter your message here.." cols="50" rows="10" class="form-control{{ $errors->has('message') ? ' is-invalid' : '' }}" name="message" required></textarea>
+        
+                                        @if ($errors->has('message'))
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $errors->first('message') }}</strong>
+                                            </span>
+                                        @endif
+                                    </div>
+                                </div>
+                                
+                                <div class="form-group row mb-0">
+                                    <div class="col-md-8 offset-md-4">
+                                        <button type="submit" class="btn btn-warning">
+                                            {{ __('Send SMS') }}
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
                 </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+            </div>
             </div>
         </div>
     </body>
